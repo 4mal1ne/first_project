0d57cff04e4eb0293d1373565e4bded1b4930e0e
4mal1ne <parieiko.vlad@gmail.com>
2021-08-24 01:12:56 +0300

    Added functions to display web pages when buttons are pressed

--- views_works/views.py
+++ views_works/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Event
+
+
+def home_page(request):
+    data = Event.objects.filter(period__event__title='Старость')
+    return render(request, 'views_works/home_page.html', {"data": data})
+
+
+def childhood(request):
+    data = Event.objects.filter(period__title="Детство")
+    return render(request, 'views_works/childhood.html', {"data": data})
+
+
+def youth(request):
+    data = Event.objects.filter(period__title="Молодость")
+    return render(request, 'views_works/youth.html', {"data": data})
+
+
+def old_age(request):
+    data = Event.objects.filter(period__title="Старость")
+    return render(request, 'views_works/old_age.html', {"data": data})
